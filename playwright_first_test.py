c352692387afd454128b79d7d35e57a92273253c
viktor_ozerchuk <ozerchuk22@ukr.net>
2024-08-08 21:37:37 +0200

    initial commit/first test

---
+++ playwright_first_test.py
@@ -0,0 +1,23 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://uat.maxsipapps.com/")
+    page.get_by_placeholder("Email Address").fill("mir@mail.com")
+    page.get_by_placeholder("Password").click()
+    page.get_by_placeholder("Password").fill("Ivan2910")
+    page.get_by_role("button", name="Sign in").click()
+    page.pause()
+    print("Your first test is successful")
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
